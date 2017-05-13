948ddb33b71fbc359c8b433bd1ec06657adb979f
annprisyazhnyuk <paoletta0011@gmail.com>
2017-05-13 16:35:35 +0300

    setup project and created gitignore

---
+++ wp-content/themes/vdart/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * vdart Theme Customizer
+ *
+ * @package vdart
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function vdart_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'vdart_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function vdart_customize_preview_js() {
+	wp_enqueue_script( 'vdart_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'vdart_customize_preview_js' );
